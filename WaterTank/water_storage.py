e8679dd6b62c143cc8dfd2dc5399749bddd9a7a1
Jayashree <mail2vjayashree@gmail.com>
2026-04-16 12:53:42 +0530

    Water Tank problem solution two pointer approach

--- WaterTank/water_storage.py
+++ WaterTank/water_storage.py
@@ -1,5 +1,6 @@
 """Water Tank Problem brute force"""
-def trapwater(block_heights):
+'''
+def water_storage(block_heights):
     n = len(block_heights)
     units = 0
     for height in range(n):
@@ -7,7 +8,22 @@ def trapwater(block_heights):
         right_max = max(block_heights[height:]) 
         units += min(left_max, right_max) - block_heights[height]
     return units
+'''
+"""Two pointer"""
+def water_storage(block_heights):
+    left, right = 0, len(block_heights) - 1
+    left_max = right_max = units = 0
+    while left < right:
+        if block_heights[left] < block_heights[right]:
+            left_max = max(left_max, block_heights[left])
+            units += left_max - block_heights[left]
+            left += 1
+        else:
+            right_max = max(right_max, block_heights[right])
+            units += right_max - block_heights[right]
+            right -= 1
+    return units
 
 block_height = [0, 4, 0, 0, 0, 6, 0, 6, 4, 0]
 print(f"Input: {block_height}")
-print(f"Output:     {trapwater(block_height)} Units")
+print(f"Output: {water_storage(block_height)} Units")
